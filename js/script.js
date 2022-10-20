18ff61e66b72331c3108904b6a446a7d5f672be1
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-20 13:52:47 +0200

    add files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 "use strict"
+
+const team = [
+    {
+        name: 'Waynett Bennet',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'wlater-gordon-office-manager.jpg'
+    },
+];
+
+for(let value of team){
+    console.log(value);
+    for(let key in value) {
+        console.log(value[key]);
+    }
+}
+
+
+
+
+
+
+
+function addPerson(obj) {
+    obj.name;
+    obj.surname;
+
+}
