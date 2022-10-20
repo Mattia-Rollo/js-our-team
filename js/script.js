914477657a0411a13fc382e9411297a72493d903
Mattia-Rollo <ste.tia@hotmail.it>
2022-10-20 14:48:49 +0200

    add funciton addCard

--- js/script.js
+++ js/script.js
@@ -9,22 +9,44 @@ const team = [
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'angela-caroll-editor.jpg'
+        image: 'angela-caroll-chief-editor.jpg'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'wlater-gordon-office-manager.jpg'
+        image: 'walter-gordon-office-manager.jpg'
     },
 ];
 
+// console.log(team[0].name);
+
+const wrapper = document.getElementById('wrapper');
+
 for(let value of team){
-    console.log(value);
+    // console.log(value);
     for(let key in value) {
-        console.log(value[key]);
+        // console.log(value[key]);
+        // wrapper.innerHTML += " " + value[key];
     }
+    addCard(value);
 }
 
+function addCard(obj) {
+    obj.name;
+    obj.role;
+    obj.image;
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const image = document.createElement('img');
+    image.classList.add('card-img-top');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    image.src = '/img/' + obj.image ;
+    card.appendChild(image);
+    card.appendChild(cardBody);
+    wrapper.appendChild(card);
+    cardBody.innerHTML = obj.name + ' ' + obj.role;
+}
 
 
 
